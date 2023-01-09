51a1450d340246870efef36fb0e0ba4561c36071
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-10 00:53:31 +0530

    Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  isActivated = false;
+  activateSubscription: Subscription;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activateSubscription = this.userService.activated.subscribe(
+      (data: boolean) => {
+        this.isActivated = data;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.activateSubscription.unsubscribe();
+  }
 }
---
+++ src/app/user.service.ts
@@ -0,0 +1,10 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  activated = new Subject<boolean>();
+  constructor() {}
+}
